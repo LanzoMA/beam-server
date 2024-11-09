9a11799d0339a5748ca3081b93d27d25239f39e4
LanzoMA <lanzo.ma9@gmail.com>
2024-11-09 08:48:22 +0000

    implemented function getUserIdByEmail

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,18 +1,23 @@
 import mongoose, { Schema } from "mongoose";
 
-interface User {
+interface User extends mongoose.Document {
     email: string;
     password: string;
 }
 
-const userSchema = new Schema({
+const userSchema: Schema<User> = new Schema<User>({
     email: { type: String, required: true, lowercase: true, unique: true },
     password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', userSchema);
+const User: mongoose.Model<User> = mongoose.model<User>('User', userSchema);
 
 export const getUsers = async () => User.find();
+
+export const getUserIdByEmail = async (email: string) => {
+    const user = await User.findOne({ email }).exec();
+    return user?._id;
+}
 export const createUser = async (user: User) => new User(user).save();
 export const updateUserEmailById = async (id: string, email: string) => User.findByIdAndUpdate(id, { email });
 export const updateUserPasswordById = async (id: string, password: string) => User.findByIdAndUpdate(id, { password });
