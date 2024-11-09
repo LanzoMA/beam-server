eb24780c478e230bed0600d2cb1d273f45940ae4
LanzoMA <lanzo.ma9@gmail.com>
2024-11-09 08:58:06 +0000

    renamed to user model to avoid confusion

--- src/db/users.ts
+++ src/db/users.ts
@@ -15,15 +15,15 @@ const userSchema: Schema<User> = new Schema<User>({
     password: { type: String, required: true },
 });
 
-const User: mongoose.Model<User> = mongoose.model<User>('User', userSchema);
+const UserModel: mongoose.Model<User> = mongoose.model<User>('User', userSchema);
 
-export const getUsers = async () => User.find();
+export const getUsers = async () => UserModel.find();
 
 export const getUserIdByEmail = async (email: string) => {
-    const user = await User.findOne({ email }).exec();
+    const user = await UserModel.findOne({ email }).exec();
     return user?._id;
 }
-export const createUser = async (user: UserStructure) => new User(user).save();
-export const updateUserEmailById = async (id: string, email: string) => User.findByIdAndUpdate(id, { email });
-export const updateUserPasswordById = async (id: string, password: string) => User.findByIdAndUpdate(id, { password });
-export const deleteUserById = async (id: string) => User.findByIdAndDelete(id);
+export const createUser = async (user: UserStructure) => new UserModel(user).save();
+export const updateUserEmailById = async (id: string, email: string) => UserModel.findByIdAndUpdate(id, { email });
+export const updateUserPasswordById = async (id: string, password: string) => UserModel.findByIdAndUpdate(id, { password });
+export const deleteUserById = async (id: string) => UserModel.findByIdAndDelete(id);
