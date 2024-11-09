ac4b8c4dda3c1d6ea2020840e065984e23dc56b9
LanzoMA <lanzo.ma9@gmail.com>
2024-11-09 08:16:28 +0000

    removed default endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/', router());
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
