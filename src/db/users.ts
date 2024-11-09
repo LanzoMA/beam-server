fddf8f23879a048ecb932838803f8a65edcad02e
LanzoMA <lanzo.ma9@gmail.com>
2024-11-09 08:04:52 +0000

    created update password service

--- src/db/users.ts
+++ src/db/users.ts
@@ -10,4 +10,5 @@ const User = mongoose.model('User', userSchema);
 export const getUsers = async () => User.find();
 export const createUser = async (email: string, password: string) => new User({ email, password }).save();
 export const updateUserEmailById = async (id: string, email: string) => User.findByIdAndUpdate(id, { email });
+export const updateUserPasswordById = async (id: string, password: string) => User.findByIdAndUpdate(id, { password });
 export const deleteUserById = async (id: string) => User.findByIdAndDelete(id);
