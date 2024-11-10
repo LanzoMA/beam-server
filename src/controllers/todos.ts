c41336f47bb6cf24d890fe20153e7927370b049a
LanzoMA <lanzo.ma9@gmail.com>
2024-11-10 07:48:25 +0000

    added updateTodoHandler

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { createTodo, getTodos } from '../db/todos';
+import { checkTodoById, createTodo, getTodos, uncheckTodoById, updateTodoDescriptionById, updateTodoTitleById } from '../db/todos';
 import { getUserIdByEmail } from '../db/users';
 import { Schema } from 'mongoose';
 
 export const getTodosHandler = async (req: Request, res: Response): Promise<void> => {
     const todos = await getTodos();
     res.json(todos);
-}
+};
 
 export const createTodoHandler = async (req: Request, res: Response): Promise<void> => {
     const { email, password, title, description } = req.body;
@@ -27,12 +27,40 @@ export const createTodoHandler = async (req: Request, res: Response): Promise<vo
         console.log(error);
         res.sendStatus(400);
     }
-}
+};
 
 export const updateTodoHandler = async (req: Request, res: Response): Promise<void> => {
+    const { email, password, id, title, description, checked } = req.body;
 
-}
+    if (!email || !password) {
+        res.status(400).send('Email and password was not found');
+        return;
+    }
+
+    if (!id) {
+        res.status(400).send('No todo given to change');
+        return;
+    }
+
+    if (title) {
+        await updateTodoTitleById(id, title);
+    }
+
+    if (description) {
+        await updateTodoDescriptionById(id, description);
+    }
+
+    if (checked === true) {
+        await checkTodoById(id);
+    }
+
+    if (checked === false) {
+        await uncheckTodoById(id);
+    }
+
+    res.send('Successfully updated todo');
+};
 
 export const deleteTodoHandler = async (req: Request, res: Response): Promise<void> => {
 
-}
+};
